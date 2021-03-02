bb8783a63ad8e1bab4fa32986112be2d98ebfbbf
Bryce <themaughan1.0@gmail.com>
2021-03-01 23:39:37 -0700

    live txt update

--- week07/Multiple.js
+++ week07/Multiple.js
@@ -1,10 +1,4 @@
-// Create an object:
 
-
-//function update() {
-//    document.getElementById('live_update').innerHTML = this.value;
-//}
-//var multi_num = document.getElementById("textarea_1").firstChild;
 
 // Create objects here:
 
@@ -18,7 +12,6 @@ var person = {
       return this.firstName + " " + this.lastName;
     }
 };
-
 class Multiple_Num {
     constructor(multi_num, loop_num) {
         this.multiple = multi_num;
@@ -28,8 +21,8 @@ class Multiple_Num {
             return "You Entered: " + this.multiple;
         }
         this.display_2 = function() {
-            let text = "";
-            for( i=1; i<=this.loop_num; i++) {
+            var text = "";
+            for(var i=1; i<=this.loop_num; i++) {
                 text +=  i + " * " + this.multiple + " = " + i*this.multiple + "<br>";
             }
             return text
@@ -42,30 +35,39 @@ class Multiple_Num {
   //document.getElementById("demo").innerHTML = person.fullName();
   //document.getElementById("Calculator").innerHTML =;
 //let num_inputs = [];
-
-var addNumber = (ev)=>{
+/*
+const addNumber = (ev)=>{
     ev.preventDefault();
-
-    var number = {
-        textarea_1 : document.getElementById('textarea_1').value
-    }
+    var number = document.getElementById('textarea_1').value;
     var c = new Multiple_Num(number, 10);
     document.getElementById("output_1").innerHTML = c.display_1();
-    
+    };
     //num_inputs.push(number);
     //document.forms[0].reset();
-      //const click_display =  {
-        //int : document.getElementById("output_2").innerHTML = c.display_2()
-      //};
-}
+
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', addNumber);
     document.getElementById('btn_2').addEventListener('click', addNumber);
 });
 
+function addNumber() {
+    var number = document.getElementById('textarea_1').value;
+    return number;
+}
+*/
 
-              
+textarea_1.oninput = function() {
+    output_1.innerHTML = textarea_1.value;
+  }
 
+var c = new Multiple_Num(9, 10);
+
+document.getElementById("output_1").innerHTML = c.display_1();
+
+
+function click_display() {
+    document.getElementById("output_2").innerHTML = c.display_2();
+}    
 function deleteItems() {
     var x = document.getElementById("output_2");
     while (x.hasChildNodes()) {
