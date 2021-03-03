707d5f210c4457e98fe777edf8613ef6ef207af8
Bryce <themaughan1.0@gmail.com>
2021-03-03 11:38:32 -0700

    Multiple calculator is done, live updates in class

--- week07/Multiple.js
+++ week07/Multiple.js
@@ -1,100 +1,48 @@
-
-//import React from 'react';
-// Create objects here:
-
-
-
-var person = {
-    firstName: "John",
-    lastName : "Doe",
-    id     : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-};
-
-
-// Display objects for Multiples Calculator here:
-  
-
-  //document.getElementById("demo").innerHTML = person.fullName();
-//let num_inputs = [];
-/*
-
-    var number = document.getElementById('textarea_1').value;
-    var c = new Multiple_Num(number, 10);
-    document.getElementById("output_1").innerHTML = c.display_1();
-    };
-    //num_inputs.push(number);
-    //document.forms[0].reset();
-
-
-*/
-
-const addNumber = (ev)=>{
+// Multiple Calculator - Created for testing code and learning!
+const deleteItems = (ev)=>{// When mouse clicks on button_2
     ev.preventDefault();
-    document.getElementById("output_2").innerHTML = c.display_2();
-
-};
-const deleteItems = (ev)=>{
-    ev.preventDefault();
-    var x = document.getElementById("output_2");
+    var x = document.getElementById("output_1");
+    var y = document.getElementById("output_2");
     while (x.hasChildNodes()) {
         x.removeChild(x.firstChild);
     }
+    while (y.hasChildNodes()) {
+        y.removeChild(y.firstChild);
+    }
+    document.forms[0].reset();
+    
 };
-
-const live_update = (ev)=>{
-    function update(textarea_1) { // This should update class parameters...
-        num = parseFloat(textarea_1, 10);
-        //document.getElementById("test").innerHTML = num;
-        document.getElementById("test_2").innerHTML = num;
-        //This is a class:
-        class Multiple_Num {
-          constructor(multi_num, loop_num) {
-              this.multiple = multi_num;
-              this.loop_num = loop_num;
+const live_update = (ev)=>{ // When Key event happens, execute this code:
+    ev.preventDefault();
+    //Start of Class:
+    class Multiple_Num {
+        constructor(multi_num, loop_num) {
+            this.multiple = multi_num;
+            this.loop_num = loop_num;
       
-              this.display_1 = function(){
-                  return "You Entered: " + this.multiple;
-              }
-              this.display_2 = function() {
-                  var text = "";
-                  for(var i=1; i<=this.loop_num; i++) {
-                      text +=  i + " * " + this.multiple + " = " + i*this.multiple + "<br>";
-                  }
-                  return text
-              }
-          }// End of class
+            this.display_1 = function(){
+                return "You Entered: " + this.multiple;
+            }
+            this.display_2 = function() {
+                var text = "";
+                for(var i=1; i<=this.loop_num; i++) {
+                    text +=  i + " * " + this.multiple + " = " + i*this.multiple + "<br>";
+                }
+                return text
+            }
         }
-      };
+    }// End of class
+    num = textarea_1.value; // set num value
+    //document.getElementById("test").innerHTML = num; //Only uncomment for testting
+    var c = new Multiple_Num(num, 10); //Create object and pass parameters
+    document.getElementById("output_1").innerHTML = c.display_1(); //Display object 1st display
+    document.getElementById("output_2").innerHTML = c.display_2(); //Display object 2nd display
 }
-var c = new Multiple_Num(9, 10);
+
   
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', addNumber);
-    document.getElementById('btn_2').addEventListener('click', deleteItems);
-    document.getElementById('textarea_1').addEventListener('keyup', live_update);
+document.addEventListener('DOMContentLoaded', ()=>{ //Listen for events after page loads:
+    
+    document.getElementById('btn_1').addEventListener('click', deleteItems);// Listen for mouse click
+    document.getElementById('textarea_1').addEventListener('keyup', live_update);// Listen for key release
 });
-
-///////// This is for live updating user input:
-var form = document.forms.calculator;
-form.textarea_1.oninput = live;
-
-
-//Object.setPrototypeOf(Multiple_Num.prototype, number);
-
-function live() { //- This updates text... for now
-  test.innerHTML = textarea_1.value;
-  num = textarea_1.value;
-};
-
-
-
-
-
-document.getElementById("output_1").innerHTML = c.display_1();
-
-live();
-update();
