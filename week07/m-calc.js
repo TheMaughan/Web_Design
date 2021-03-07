aecbb046b6d6b5fa448f4d5d9a2e00f09df5f23a
Bryce <themaughan1.0@gmail.com>
2021-03-06 19:50:02 -0700

    fixed null input validation

--- week07/m-calc.js
+++ week07/m-calc.js
@@ -8,15 +8,42 @@ const calculate = (ev)=>{
     let t = term.value;
     let amnt = amount.value;
 
+
+    
     // Doing the Math:
     t *= 12;
     a /= 1200;
     amnt *= (a * ((1 + a) ** t)) / (((1 + a) ** t) - 1);
     let payment = amnt.toFixed(2);
 
-    //\B looks for a word boundary, ? says what to look for, \d looks for 3 digits in a row:
-    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById('payment').innerHTML = `Monthly Payment = $${payment}`
+    if (a == null || isNaN(a) || a == " "){
+        document.getElementById('payment').innerHTML = "APR input is required";
+        document.getElementById('apr_error').innerHTML = "APR must be 0% - 25%";
+        document.getElementById('apr_error').style.textShadow = '0px 0px 10px red';
+        document.getElementById('apr').style.boxShadow = '0px 0px 10px red inset';
+        document.getElementById("apr").focus();
+    } else if (t == null || isNaN(t) || t == " "){
+        document.getElementById('payment').innerHTML = "Lone Term input is required";
+        document.getElementById('term_error').innerHTML = "Enter an number that is 0 - 40";
+        document.getElementById('term_error').style.textShadow = '0px 0px 10px red';
+        document.getElementById('term').style.boxShadow = '0px 0px 10px red inset';
+        document.getElementById("term").focus();
+    } else if (amnt == null || isNaN(amnt) || amnt == " "){
+        document.getElementById('payment').innerHTML = "Enter a Lone Amount";
+        document.getElementById('amount_error').innerHTML = "Enter Your Lone Amount";
+        document.getElementById('amount_error').style.textShadow = '0px 0px 10px red';
+        document.getElementById('amount').style.boxShadow = '0px 0px 10px red inset';
+        document.getElementById("amount").focus();
+    } else if (payment == null || isNaN(payment) || payment == " "){
+        document.getElementById('payment').innerHTML = "Calculation Error, recheck your numbers, no letters allowed";
+        document.getElementById('payment').style.textShadow = '0px 0px 10px red';
+    } else {
+        //\B looks for a word boundary, ? says what to look for, \d looks for 3 digits in a row:
+        payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        document.getElementById('payment').innerHTML = `Monthly Payment = $${payment}`
+        document.getElementById('payment').style.textShadow = 'none';
+    }
+    
     
 }
 
@@ -66,6 +93,7 @@ const update = (ev)=>{ // When Key-Release event happens, execute this code:
     if (a < 0.00 || a > 25.00){
         document.getElementById('apr').style.boxShadow = '0px 0px 10px red inset';
         document.getElementById('apr_error').innerHTML = "You entered: " + a + "%, APR must be 0% - 25%";
+        document.getElementById('apr_error').style.textShadow = '0px 0px 10px red';
         document.getElementById("apr").value = null;
         document.getElementById("apr").focus();
     } else if (a.includes(".")){ // Decimal input validation, only 2 decimal places allowed:
@@ -74,25 +102,30 @@ const update = (ev)=>{ // When Key-Release event happens, execute this code:
         if(numb!=null && numb.length>2){
             document.getElementById('apr').style.boxShadow = '0px 0px 10px red inset';
             document.getElementById('apr_error').innerHTML = a + "% - Only 2 decimal places allowed";
+            document.getElementById('apr_error').style.textShadow = '0px 0px 10px red';
             //document.getElementById("apr").value = null;
         } else {
             document.getElementById('apr_error').innerHTML = a + "%";
             document.getElementById('apr').style.boxShadow = 'none';
+            document.getElementById('apr_error').style.textShadow = 'none';
         }
     } else {
         document.getElementById('apr_error').innerHTML = a + "%";
         document.getElementById('apr').style.boxShadow = 'none';
+        document.getElementById('apr_error').style.textShadow = 'none';
     }
     
     // Term input validation
     if (t < 0 || t > 40){
         document.getElementById('term').style.boxShadow = '0px 0px 10px red inset';
         document.getElementById('term_error').innerHTML = "You Entered: " + t + ", enter an input that is 0 - 40";
+        document.getElementById('term_error').style.textShadow = '0px 0px 10px red';
         document.getElementById("term").value = null;
         document.getElementById("term").focus();
     } else {
         document.getElementById('term_error').innerHTML = t + " years";
         document.getElementById('term').style.boxShadow = 'none';
+        document.getElementById('term_error').style.textShadow = 'none';
         //t = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 0)) : t;
     }
     
@@ -104,14 +137,17 @@ const update = (ev)=>{ // When Key-Release event happens, execute this code:
         if(numb!=null && numb.length>2){
             document.getElementById('amount').style.boxShadow = '0px 0px 10px red inset';
             document.getElementById('amount_error').innerHTML = `$${amnt} - Only 2 decimal places allowed`
+            document.getElementById('amount_error').style.textShadow = '0px 0px 10px red';
             //document.getElementById("amount").value = null;
         } else {
             document.getElementById('amount_error').innerHTML = `$${amnt}`
             document.getElementById('amount').style.boxShadow = 'none';
+            document.getElementById('amount_error').style.textShadow = 'none';
         }
     } else {
         document.getElementById('amount_error').innerHTML = `$${amnt}`
         document.getElementById('amount').style.boxShadow = 'none';
+        document.getElementById('amount_error').style.textShadow = 'none';
     }
     //document.getElementById('amount_error').innerHTML = "Amount Entered: $" + amnt;
 }
