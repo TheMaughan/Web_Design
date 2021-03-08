4236318643da1107a57267267f26345169917f02
Bryce <themaughan1.0@gmail.com>
2021-03-08 11:26:27 -0700

    updates

--- week08/validate.js
+++ week08/validate.js
@@ -1,49 +1,198 @@
-// Multiple Calculator - Created for testing code and learning!
-const deleteItems = (ev)=>{// When mouse clicks on button_2
-    ev.preventDefault();
-    var x = document.getElementById("output_1");
-    var y = document.getElementById("output_2");
-    while (x.hasChildNodes()) {
-        x.removeChild(x.firstChild);
-    }
-    while (y.hasChildNodes()) {
-        y.removeChild(y.firstChild);
-    }
-    document.forms[0].reset();
-    document.getElementById('textarea_1').focus()
+//JavaScript File
+// Morgage Calculator - Created for Week 07 assignment
+const submit = (ev)=>{
+  ev.preventDefault();
+  //Get the input node values:
+  let fn, ln, add, pho, card, date;
+  fn = first_name.value;
+  ln = last_name.value;
+  add = address.value;
+  pho = phone.value;
+  card = credit_card.value;
+  date = exp_date.value;
+
+
+
     
-};
-const live_update = (ev)=>{ // When Key event happens, execute this code:
-    ev.preventDefault();
-    //Start of Class:
-    class Multiple_Num {
-        constructor(multi_num, loop_num) {
-            this.multiple = multi_num;
-            this.loop_num = loop_num;
-      
-            this.display_1 = function(){
-                return "You Entered: " + this.multiple;
-            }
-            this.display_2 = function() {
-                var text = "";
-                for(var i=1; i<=this.loop_num; i++) {
-                    text +=  i + " * " + this.multiple + " = " + i*this.multiple + "<br>";
-                }
-                return text
-            }
-        }
-    }// End of class
-    num = textarea_1.value; // set num value
-    //document.getElementById("test").innerHTML = num; //Only uncomment for testting
-    var c = new Multiple_Num(num, 10); //Create object and pass parameters
-    document.getElementById("output_1").innerHTML = c.display_1(); //Display object 1st display
-    document.getElementById("output_2").innerHTML = c.display_2(); //Display object 2nd display
+  // Doing the Math:
+    
+
+  if (fn.trim() == ''){
+    document.getElementById('validate').innerHTML = "Missing First Name";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('first_name').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("first_name").focus();
+  } else if (ln.trim() == ''){
+    document.getElementById('validate').innerHTML = "Missing Last Name";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('last_name').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("last_name").focus();
+  } else if (add.trim() == ''){
+    document.getElementById('validate').innerHTML = "Shipping Address is Required";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('address').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("address").focus();
+  } else if (pho.trim() == ''){
+    document.getElementById('validate').innerHTML = "Phone Number is Required";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('phone').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("phone").focus();
+  } else if (card.trim() == ''){
+    document.getElementById('validate').innerHTML = "Credit Card Payment is Required";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('credit_card').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("credit_card").focus();
+  } else if (date.trim() == ''){
+    document.getElementById('validate').innerHTML = "Card Experation Date is Missing";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('exp_date').style.boxShadow = '0px 0px 10px red inset';
+    document.getElementById("exp_date").focus();
+  } else {
+    //Submit and validation affirmation display:
+    document.getElementById('validate').innerHTML = "Thanks for your purchase " + fn + "!";
+    //clear text validation shadow:
+    document.getElementById('validate').style.textShadow = 'none';
+    //clear input box shadows:
+    document.getElementById('first_name').style.boxShadow = 'none';
+    document.getElementById('last_name').style.boxShadow = 'none';
+    document.getElementById('address').style.boxShadow = 'none';
+    document.getElementById('phone').style.boxShadow = 'none';
+    document.getElementById('credit_card').style.boxShadow = 'none';
+    document.getElementById('exp_date').style.boxShadow = 'none';
+  }
+    
+    /*} else if (payment == null || isNaN(payment) || payment == " "){
+        document.getElementById('payment').innerHTML = "Calculation output, recheck your numbers, no letters allowed";
+        document.getElementById('payment').style.textShadow = '0px 0px 10px red';
+    } else {
+        //\B looks for a word boundary, ? says what to look for, \d looks for 3 digits in a row:
+        payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        document.getElementById('payment').innerHTML = `Monthly Payment = $${payment}`
+        document.getElementById('payment').style.textShadow = 'none';
+    }*/
+    
+    
 }
 
-  
+const clear = (ev)=>{// When mouse clicks on 'clear' button:
+  ev.preventDefault();
+  //reset all user input using jquery:
+  $('#morgage_calculator')[0].reset();
+  $(':input').val('');
+  $('#item').removeClass('');
+  //clear validate notifications
+  const valid = document.getElementById("validate");
+  while (valid.hasChildNodes()) {
+    document.getElementById('validate').innerHTML = "";
+    //valid.removeChild(valid.firstChild);
+  }
+  const v_card = document.getElementById("vCard");
+  while (v_card.hasChildNodes()) {
+    document.getElementById('vCard').innerHTML = "";
+    //valid.removeChild(valid.firstChild);
+  }
+
+    /*
+    while (.hasChildNodes()) {
+        .removeChild(.firstChild);
+        //document.getElementById('first_name').inputField.value = " ";
+    }*/
+    //input.value = null;
+    //document.forms[0].reset();
+  document.getElementById("first_name").focus(); // put cursor inside first_name input Field
+};
+
+
+
+
+function ValidateInput() {
+
+  var ccNum = document.getElementById('credit_card').value;
+  var phone_in = document.getElementById('phone').value;
+  //var visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+  //var mastercardRegEx = /^(?:5[1-5][0-9]{14})$/;
+  //var amexpRegEx = /^(?:3[47][0-9]{13})$/;
+  //var discovRegEx = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+  var testing = /^\d{4}-\d{4}-\d{4}-\d{4}$/; // Auto hyphen works, the other cards are to complex...
+  var pho = /^\d{3}-\d{3}-\d{4}/;
+  var isValid = false;
+
+  var v_select = document.getElementById('visa').checked;
+  var m_select = document.getElementById('master').checked;
+  var d_select = document.getElementById('discover').checked;
+  var a_select = document.getElementById('americaExpress').checked;
+  // var t_select = document.getElementById('test').checked;
+
+  if (testing.test(ccNum) && v_select) {
+    isValid = true;
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Valid Visa Card Number";
+  } else if(testing.test(ccNum) && m_select) {
+    isValid = true;
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Valid Master Card Number";
+  } else if(testing.test(ccNum) && d_select) {
+    isValid = true;
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Valid Discover Card Number";
+  } else if(testing.test(ccNum) && a_select) {
+    isValid = true;
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Valid American Express Card Number";
+  } else if(v_select == false || m_select == false || d_select == false || a_select == false){
+    document.getElementById('validate').innerHTML = "Select Your Credit Card Type";
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+  }
+  //Validate Phone Input
+  if(pho.test(phone_in)){
+    document.getElementById('validate').style.textShadow = '0px 0px 10px red';
+    document.getElementById('validate').innerHTML = "Enter a Valid Phone Number = ###-###-####";
+  } else {
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Phone Number Accepted";
+  }
+  /* else if(testing.test(ccNum) && t_select) {
+    isValid = true;
+    document.getElementById('validate').style.textShadow = '0px 0px 10px green';
+    document.getElementById('validate').innerHTML = "Card Input Testing with a positive number";
+  }*/
+
+
+  if(isValid) {
+    document.getElementById('vCard').innerHTML = "Card Accepted";
+    document.getElementById('vCard').style.textShadow = '0px 0px 10px green';
+  } else {
+    document.getElementById('vCard').innerHTML = "Card Denied";
+    document.getElementById('vCard').style.textShadow = '0px 0px 10px red';
+    document.getElementById('credit_card').focus();
+  }
+}
+
+//For Credit Card, Live update on input... to bad I couldn't get this to work for the phone number.
+function addHyphen(element) { // Add Hyphen if 'test' is selected
+  let ele = document.getElementById(element.id);
+    ele = ele.value.split('-').join('');    // Remove dash (-) if mistakenly entered.
+
+    let finalVal = ele.match(/.{1,4}/g).join('-');
+    document.getElementById(element.id).value = finalVal;
+}
+/*
+function phoneHyphen(f){
+  f_val = f.value.replace(/\D[^\.]/g, "");
+  f.value = f_val.slice(0,3)+"-"+f_val.slice(3,6)+"-"+f_val.slice(6);
+}*/
+
+function phoneHyphen(slash){
+  //let s = 
+  var date = document.getElementById(slash.id).value;
+  date = date.toString().replace("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+  document.getElementById(slash.id).value = date;
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{ //Listen for events after page loads:
     
-    document.getElementById('btn_1').addEventListener('click', deleteItems);// Listen for mouse click
-    document.getElementById('textarea_1').addEventListener('keyup', live_update);// Listen for key release
+  document.getElementById('submit').addEventListener('click', submit);// Listen for mouse click
+  document.getElementById('clear').addEventListener('click', clear);// Listen for mouse click  
+  //document.getElementById('last_name').addEventListener('keyup', update);// Listen for key release
+  //document.getElementById('add_1').addEventListener('keyup', update);// Listen for key release
 });
