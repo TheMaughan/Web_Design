f5391cb554de3b678cec1ad230cb56a0d9ff9a30
Bryce <themaughan1.0@gmail.com>
2021-04-03 17:20:25 -0600

    Doing Math for items, not done yet...

---
+++ week08/receipt.js
@@ -0,0 +1,28 @@
+// add selection to Total:
+function addTotal(item){
+  let i_1 = 1200.50;
+  let i_2 = 2243.75;
+  let i_3 = 324200.00;
+  let i_4 = 1000000000.00;
+
+
+
+  let addItems = 0;
+
+  switch(item){
+    case 1 : document.getElementById('total').innerHTML = 'item_1'; break;//addItems + i_1;
+    case 2 : document.getElementById('total').innerHTML = 'item_1'; break;
+    case 3 : document.getElementById('total').innerHTML = 'item_1'; break;
+    case 4 : document.getElementById('total').innerHTML = 'item_1'; break;
+    default : document.getElementById('total').innerHTML = '$0.00';
+  }
+
+  document.getElementById('total').innerHTML = '$' + addItems;
+
+  //let total = document.getElementById('total').innerHTML = '$' + currentTotal;
+  //document.getElementById('item_2').value;
+  //document.getElementById('item_3').value;
+  //document.getElementById('item_4').value;
+
+
+}
